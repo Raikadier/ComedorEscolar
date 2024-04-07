683ae5e563373206b9ee8325c0c2e904dae6ceb6
Raikadier <138398472+Raikadier@users.noreply.github.com>
2024-04-07 18:38:17 -0500

    Add files via upload

---
+++ ComedorUniversitario/ComedorEscolar 1.0/src/comedorescolar/SistemaDeAsistencia.java
@@ -0,0 +1,13 @@
+package comedorescolar;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class SistemaDeAsistencia {
+
+    private List<Estudiante> estudiantes;
+
+    public boolean registrarAsistencia(Estudiante estudiante, LocalDate fecha) {
+        return true;
+    }
+}
