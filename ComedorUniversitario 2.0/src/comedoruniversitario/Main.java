fa610171a6d956a0606f46100afc0ec676e05d11
lauraltahona <lauraaltahona01@gmail.com>
2024-04-21 19:34:46 -0500

    Elimine carpetas

---
+++ ComedorUniversitario 2.0/src/comedoruniversitario/Main.java
@@ -0,0 +1,16 @@
+
+package comedoruniversitario;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Estudiante juan = new Estudiante("David Barcelo", 1067594372, 3, LocalDate.now());
+        
+        
+        
+        
+    }
+    
+}
