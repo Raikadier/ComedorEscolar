27ce645bb39171f2cdea4385125a6993593bbfe0
Raikadier <138398472+Raikadier@users.noreply.github.com>
2024-06-17 08:36:26 -0500

    Delete ComedorEscolar-main/ComedorEscolarMaven directory

--- ComedorEscolar-main/ComedorEscolarMaven/src/main/java/com/mycompany/comedorescolarmaven/logica/RegistroEntrega.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.comedorescolarmaven.logica;
-
-public interface RegistroEntrega {
-
-    public boolean registrarEntrega(Entrega entrega);
-
-    public void mostrarLista();
-}
